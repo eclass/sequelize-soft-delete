23f80407406fd65d005affaa1d93529f5657c977
Leonardo Gatica <lgatica@protonmail.com>
2018-05-07 16:16:14 -0300

    fix(model): fix set model method

--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 'use strict'
 
 class SequelizeSoftDelete {
-  constructor (options = {}) {
-    this.deleted = options.deleted || true
-  }
   /**
-   * soft delete
-   * Set deleted to true
-   * @param {Object} options Options to filter query
-   * @return {Number} Total updated
+   * Sequelize Model
+   * @external {Model} http://docs.sequelizejs.com/class/lib/model.js~Model.html
    */
-  async softDelete (options) {
-    const docs = await this.update({ deleted: this.deleted }, options)
-    return docs.length
+  /**
+   * The softDelete options
+   * @typedef {Object} SoftDeleteOptions
+   * @property {String} [field=deleted] The field of deleted
+   * @property {Any} [deleted=true] The value of state deleted
+   * etc.
+   */
+  /**
+   * Method to append softDelete method to Model
+   * @param {Model} Model Sequelize Model
+   * @param {SoftDeleteOptions} options
+   */
+  softDelete (Model, options) {
+    const defaultOptions = { field: 'deleted', deleted: true }
+    const deletedOptions = { ...options, ...defaultOptions }
+    /**
+     * soft delete
+     * Set deleted to true
+     * @param {Object} options Options to filter query
+     * @return {Number} Total updated
+     */
+    const updateDeleted = async function (options) {
+      const docs = await Model.update(
+        { deleted: deletedOptions.deleted },
+        options
+      )
+      return docs.length
+    }
+    const instanceOrModel = Model.Instance || Model
+    instanceOrModel.softDelete = updateDeleted
   }
 }
 
-/**
- * Sequelize Model
- * @external {Model} http://docs.sequelizejs.com/class/lib/model.js~Model.html
- */
-/**
- * The softDelete options
- * @typedef {Object} SoftDeleteOptions
- * @property {Any} [deleted=true] The value of state deleted
- * etc.
- */
-/**
- * Method to append softDelete method to Model
- * @param {Model} Model Sequelize Model
- * @param {SoftDeleteOptions} options
- */
-module.exports = (Model, options) => {
-  const sequelizeSoftDelete = new SequelizeSoftDelete(options)
-  Model.softDelete = sequelizeSoftDelete.softDelete
-}
+module.exports = new SequelizeSoftDelete()
